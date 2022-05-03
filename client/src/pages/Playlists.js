8c2da12a74d33355fd8a33d2273c4ac53cefed0e
Kate Hunter <iamkatehunter@gmail.com>
2022-05-03 22:45:28 +1000

    less than bare minimum playslists

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -1,7 +1,60 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+import { getCurrentUserPlaylists } from '../spotify';
+import { catchErrors } from '../utils';
+
+
 const Playlists = () => {
+    const [playlistsData, setPlaylistsData] = useState(null);
+    const [playlists, setPlaylists] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await getCurrentUserPlaylists();
+            setPlaylists(data);
+        };
+
+    catchErrors(fetchData());
+    }, []);
+
+    useEffect(() => {
+        if (!playlistsData) {
+            return;
+        }
+
+        const fetchMoreData = async () => {
+            if (playlistsData.next) {
+               const { data } = await axios.get(playlistsData.next);
+               setPlaylistsData(data); 
+            }
+        };
+
+        setPlaylists(playlists => ([
+            ...playlists ? playlists : [],
+            ...playlistsData.items
+        ]));
+
+        catchErrors(fetchMoreData());        
+        
+    }, [playlistsData]);
+
+
+
+
+
     return (
-        <h1>Playlists Page</h1>
+        <main>
+            title="Playlists" breadcrumbs="true"
+                {playlists}
+        </main>
     )
+
+
+
+    console.log(playlists);
+
+
+
 }
 
 export default Playlists;
