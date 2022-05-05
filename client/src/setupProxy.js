7e21ca797bc6d5dee9b78475066cfdfd76bad70c
alexwjohnson <xelaa@mac.com>
2022-05-05 15:38:56 +1000

    set client to point to heroku backend

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8888',
+      // target: 'http://localhost:8888',  //ToDO set environment vars to handle dev vs. prod. currently set to heroku b/e
+      target: 'https://canned-tunr.herokuapp.com/login',
       changeOrigin: true,
     })
   );
